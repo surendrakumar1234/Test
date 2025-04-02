c9b7e2e47e4db384cd7343e583cfafb7e8f509d1
surendrakumar1234 <143699443+surendrakumar1234@users.noreply.github.com>
2025-04-02 08:28:32 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -7,7 +7,7 @@ const sendNewspaperToTelegramGroup = (newspaperdate, gLink, shortenedUrl) => {
   try {
     axios
       .get(
-        `https://api.telegram.org/bot6465806242:AAH5As3iEipDMow9d8IM8bmGXya3udjEgYM/sendPhoto?chat_id=-1002499362787&message_thread_id=85&caption=*${newspaperdate}*:- ${gLink} %0A %0A*If You Want to help us View with this Link - ${shortenedUrl}*&parse_mode=markdown&photo=https://images.bhaskarassets.com/thumb/500x0/web2images/web-frontend/epaper/topBannerImg-521.png`
+        `https://api.telegram.org/bot/sendPhoto?chat_id=&message_thread_id=85&caption=*${newspaperdate}*:- ${gLink} %0A %0A*If You Want to help us View with this Link - ${shortenedUrl}*&parse_mode=markdown&photo=https://images.bhaskarassets.com/thumb/500x0/web2images/web-frontend/epaper/topBannerImg-521.png`
       )
       .then((res) => {
         console.log(res.data.ok);
@@ -55,26 +55,7 @@ function dainikBhaskarNewspaper() {
               console.log(newspaperdate);
 
               //send to telegram code
-              axios
-                .get(
-                  `https://instantlinks.co/api?api=24571ab77a4901c5e7d16fb6f2b02dbd6c5c3db2&url=${gLink}&type=2`
-                )
-                .then((res) => {
-                  // console.log(res.data.shortenedUrl);
-                  const shortenedUrl = res.data.shortenedUrl;
-                  axios
-                    .get(
-                      `https://api.telegram.org/bot6465806242:AAH5As3iEipDMow9d8IM8bmGXya3udjEgYM/sendPhoto?chat_id=@daily_newspaper_hindi24&caption=*${newspaperdate}*:- ${gLink} %0A %0A*If You Want to help us View with this Link - ${shortenedUrl}*&parse_mode=markdown&photo=https://images.bhaskarassets.com/thumb/500x0/web2images/web-frontend/epaper/topBannerImg-521.png`
-                    )
-                    .then((res) => {
-                      console.log(res.data.ok);
-                      sendNewspaperToTelegramGroup(
-                        newspaperdate,
-                        gLink,
-                        shortenedUrl
-                      );
-                    });
-                });
+              
               //send to telegram code
 
               fs.writeFile(
